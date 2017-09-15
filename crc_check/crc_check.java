67bbe1f96e69c270ee8a8db1f7e4a73197b2a51d
Divyaksh Shukla <divyaksh.shukla@gmail.com>
2017-09-15 11:34:20 +0530

    Modified wireless LAN

---
+++ crc_check/crc_check.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+class CRC16 {
+
+	public void computecrc(int gen[], int rem[]) {
+		int current = 0;
+		while(true) {
+			for (int i = 0; i < gen.length; i++) {
+				rem [current + 1] = (rem [current + 1] ^ gen [i]);
+			}
+			while (rem [current] == 0 && current != rem.length - 1) {
+				current++;
+			}
+			if((rem.length - current) < gen.length) {
+				break;
+			}
+		}
+
+		return rem;
+	}
+	public static void main(String args[]) {
+
+		String value;
+		int 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the message text:");
+		
+	}
+
+}
