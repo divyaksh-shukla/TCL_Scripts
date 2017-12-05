b4bcbfdab3563e0d25d7ad7051b50a7983309279
Divyaksh <divyaksh.shukla@hotmail.com>
2017-12-05 12:36:35 +0530

    rsa with biginteger

---
+++ rsa.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class RSA {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		BigInteger a = new BigInteger(11, 1, new Random());
+		BigInteger b = new BigInteger(11, 1, new Random());
+		BigInteger X = BigInteger.ZERO;
+		BigInteger Y = BigInteger.ZERO;
+		BigInteger N;
+		BigInteger phi;
+		BigInteger k = BigInteger.valueOf(2);
+
+		System.out.println("a  :" + a);
+		System.out.println("b  :" + a);
+
+		N = a.multiply(b);
+		phi = a.subtract(BigInteger.ONE).multiply(b.subtract(BigInteger.ONE));
+
+		System.out.println("N  :" + N);
+		System.out.println("phi:" + phi);
+
+		for (BigInteger i = BigInteger.valueOf(2) ; !(i.equals(phi)) ; i = i.add(BigInteger.ONE)) {
+			if (i.gcd(phi).equals(BigInteger.ONE)) {
+				X = new BigInteger(i.toString());
+				break;
+			}
+			//System.out.println(i);
+		}
+
+		// System.out.println("X is generated...");
+
+		for (BigInteger i = BigInteger.ONE ;; i = i.add(BigInteger.ONE)) {
+			if(X.multiply(i).remainder(phi).equals(BigInteger.ONE)) {
+				Y = new BigInteger(i.toString());
+				break;
+			}
+			//System.out.println(i);
+		}
+
+		// Y =
+
+		System.out.println("Public Key:\t" + X + "," + N);
+		System.out.println("Private Key:\t" + Y + "," + N);
+		String message;
+
+		do {
+			System.out.print("Enter the message to be encrypted:");
+			message = sc.nextLine();
+		}while(message==null);
+
+		BigInteger[] msg = new BigInteger[message.length()];
+		BigInteger[] encrypt = new BigInteger[message.length()];
+		BigInteger[] decrypt = new BigInteger[message.length()];
+
+		for(BigInteger i = BigInteger.ZERO ; !(i.equals(BigInteger.valueOf(message.length()))) ; i = i.add(BigInteger.ONE)) {
+			msg[i.intValue()] = BigInteger.valueOf((long) message.charAt(i.intValue()));
+		}
+
+		for(BigInteger i = BigInteger.ZERO ; !(i.equals(BigInteger.valueOf(message.length()))) ; i = i.add(BigInteger.ONE)) {
+			encrypt[i.intValue()] = msg[i.intValue()].modPow(X,N);
+		}
+
+		for(BigInteger i = BigInteger.ZERO ; !(i.equals(BigInteger.valueOf(message.length()))) ; i = i.add(BigInteger.ONE)) {
+			decrypt[i.intValue()] = encrypt[i.intValue()].modPow(Y,N);
+		}
+
+		System.out.println("\nEncoded Numbers:");
+		for(BigInteger i = BigInteger.ZERO ; !(i.equals(BigInteger.valueOf(message.length()))) ; i = i.add(BigInteger.ONE)) {
+			System.out.print(msg[i.intValue()] + ",");
+		}
+
+		System.out.println("\nEncrypted Numbers:");
+		for(BigInteger i = BigInteger.ZERO ; !(i.equals(BigInteger.valueOf(message.length()))) ; i = i.add(BigInteger.ONE)) {
+			System.out.print(encrypt[i.intValue()] + ",");
+		}
+
+		System.out.println("\nDecrypted Numbers:");
+		for(BigInteger i = BigInteger.ZERO ; !(i.equals(BigInteger.valueOf(message.length()))) ; i = i.add(BigInteger.ONE)) {
+			System.out.print(decrypt[i.intValue()] + ",");
+		}
+		System.out.println("\n\nDecrypted Message:");
+		for(BigInteger i = BigInteger.ZERO ; !(i.equals(BigInteger.valueOf(message.length()))) ; i = i.add(BigInteger.ONE)) {
+			System.out.print((char)(decrypt[i.intValue()].intValue()));
+		}
+		System.out.println();
+	}
+}
